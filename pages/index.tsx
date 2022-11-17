27f7f8f5375774fe4864e44dd956d6eacaf6b892
sacckey <sackeyad24@gmail.com>
2022-11-17 23:27:32 +0900

    Add infinite scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,39 @@
 import Head from 'next/head'
 import { fetchTweets } from 'lib/fetchTweets'
 import { db } from "../lib/firebase"
-import { collection, getDocs, limit, orderBy, query } from "firebase/firestore"
+import { collection, getDocs, limit, orderBy, query, startAfter, Timestamp } from "firebase/firestore"
 import Tweets from '../components/Tweets'
+import { useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroller'
 
 export default function Home({ tweets }: any) {
+  const [list, setList] = useState(tweets)
+  const [hasMore, setHasMore] = useState(tweets.length > 0)
+
+  const loadMore = async (page: any) => {
+    const lastTweetDate = Timestamp.fromDate(new Date(list.at(-1).publishedAt))
+    const q = query(collection(db, "tweets"), orderBy('publishedAt', 'desc'), startAfter(lastTweetDate), limit(30))
+    const tweetSnapshots = await getDocs(q)
+    const tweets = fetchTweets(tweetSnapshots)
+
+    if (tweets.length === 0) {
+      setHasMore(false)
+      return
+    }
+
+    setList((preList: any) => [...preList, ...tweets])
+  }
+
+  const loader =<div className="loader" key={0}>Loading ...</div>
+
   return (
     <div>
       <Head>
         <title>wipeout.ink</title>
       </Head>
-
-      <Tweets tweets={tweets} />
+      <InfiniteScroll loadMore={loadMore} hasMore={hasMore} loader={loader}>
+        <Tweets tweets={list} />
+     </InfiniteScroll>
     </div>
   )
 }
