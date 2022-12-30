4dcbd3936f55a5bd3efb969aa8bfb5f96987f0e9
sacckey <sackeyad24@gmail.com>
2022-12-30 11:38:47 +0900

    Add Google Analytics

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
+import { getAnalytics } from "firebase/analytics"
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -8,7 +9,9 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
+export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null
