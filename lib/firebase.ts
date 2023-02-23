c863a9b54c5a48d24ca71373d1074ac024cec81c
sacckey <sackeyad24@gmail.com>
2023-02-23 20:52:29 +0900

    Add development env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-import { getAuth } from "firebase/auth"
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore"
+import { connectAuthEmulator, getAuth } from "firebase/auth"
 import { getAnalytics } from "firebase/analytics"
 
 const firebaseConfig = {
@@ -16,4 +16,15 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
 export const auth = getAuth(app)
-export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null
+let analytics = null
+
+if (typeof window !== 'undefined') {
+  if (process.env.NODE_ENV === 'production') {
+    analytics = getAnalytics(app)
+  } else {
+    connectFirestoreEmulator(db, 'localhost', 8080)
+    connectAuthEmulator(auth, 'http://localhost:9099')
+  }
+}
+
+export { analytics }
