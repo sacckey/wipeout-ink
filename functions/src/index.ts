80bef71a9d33667ddb5f3be6c536016f36a3691c
sacckey <sackeyad24@gmail.com>
2023-03-10 08:22:12 +0900

    Delete comment

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -82,20 +82,19 @@ const saveTweets = async (res: any) => {
 }
 
 export const fetchTweets = functions.region('asia-northeast1').pubsub.schedule('every 5 minutes').onRun(async (context) => {
-  functions.logger.info("start!", {structuredData: true})
+  functions.logger.info("start!")
 
   const date = new Date(context.timestamp)
-  date.setMinutes(date.getMinutes() - 10)
+  date.setMinutes(date.getMinutes() - 20)
 
   try {
     const res = await searchTweets(date)
     res && await saveTweets(res)
-  } catch (e:any) {
-    console.log('error!!!!')
-    console.error(e)
+  } catch (e: any) {
+    functions.logger.error(e)
   }
 
-  functions.logger.info("end!", {structuredData: true})
+  functions.logger.info("end!")
 })
 
 const deleteTweet = async (targetTweet: any) => {
@@ -230,96 +229,6 @@ export const onDeleteLike = functions.region('asia-northeast1').firestore.docume
 
   functions.logger.info("end!", {structuredData: true})
 })
-
-// const tmpSearchTweets = async (ids: string[]) => {
-//   const client = new Client(process.env.TWITTER_BEARER_TOKEN as string)
-//   const res = await client.tweets.findTweetsById({
-//     "ids": ids,
-//     "tweet.fields": [
-//       "source",
-//       "public_metrics",
-//       "created_at"
-//     ],
-//     "expansions": [
-//       "author_id",
-//       "attachments.media_keys"
-//     ],
-//     "media.fields": [
-//       "variants",
-//     ],
-//     "user.fields": [
-//       "profile_image_url"
-//     ]
-//   })
-
-//   return res
-// }
-
-// const tmpSaveTweets = async (res: any) => {
-//   const media_key2video: {[prop: string]: any} = {}
-//   res?.includes?.media && res?.includes?.media.map((media: any) => {
-//     if (media.media_key){
-//       media_key2video[media.media_key] = media.variants.filter((variant:any) => 'bit_rate' in variant).sort((a: any, b: any) => b.bit_rate - a.bit_rate)[0].url
-//     }
-//   })
-
-//   const authors: {[prop: string]: any} = {}
-//   res?.includes?.users.map((author: any) => {
-//     authors[author.id] = author
-//   })
-
-//   const tmpSaveTweet = async (tweet: any) => {
-//     const tweetDataP = await admin.firestore().collection('tweets').doc(tweet.id).get()
-//     if (!tweetDataP.exists) {
-//       return
-//     }
-
-//     const tweetData = tweetDataP.data()
-
-//     const author = authors[tweet.author_id]
-
-//     // TODO: tweetDataを更新して保存する形式にする
-//     await admin.firestore().collection('tweets').doc(tweet.id).set({
-//       twitterUid: tweet.author_id,
-//       text: tweet.text,
-//       replyCount: tweet.public_metrics?.reply_count ?? 0,
-//       twitterLikeCount: tweet.public_metrics?.like_count ?? 0,
-//       retweetCount: tweet.public_metrics?.retweet_count ?? 0,
-//       name: author?.name,
-//       username: author?.username,
-//       profileImageUrl: author?.profile_image_url,
-//       url: `https://twitter.com/${author?.username}/status/${tweet.id}`,
-//       video: tweet.attachments?.media_keys && media_key2video[tweet.attachments?.media_keys[0]],
-//       likeCount: tweetData?.likeCount,
-//       publishedAt: admin.firestore.Timestamp.fromDate(new Date(tweet.created_at)),
-//       createdAt: tweetData?.createdAt,
-//       updatedAt: admin.firestore.FieldValue.serverTimestamp()
-//     })
-//     console.log(`saved: ${tweet.id}`)
-//   }
-
-//   res?.data && res.data.map(async (tweet: any) => {
-//     if (tweet.source === 'Nintendo Switch Share') {
-//       await tmpSaveTweet(tweet)
-//     }
-//   })
-// }
-
-// export const tmpFetchTweets = functions.region('asia-northeast1').pubsub.schedule('every 2 minutes').onRun(async (context) => {
-//   functions.logger.info("start!", {structuredData: true})
-
-//   try {
-//     const ids = ['']
-//     const res = await tmpSearchTweets(ids)
-//     res && await tmpSaveTweets(res)
-//   } catch (e:any) {
-//     console.log('error!!!!')
-//     console.error(e)
-//   }
-
-//   functions.logger.info("end!", {structuredData: true})
-// })
-
 
 export const saveTestTweets = functions.region('asia-northeast1').https.onCall(async () => {
   if (process.env.FIREBASE_DEBUG_MODE === 'false') {
