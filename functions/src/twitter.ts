cbe72fa402ef99c6c6b989c7798593ef9a2774f8
sacckey <sackeyad24@gmail.com>
2023-04-02 11:39:19 +0900

    Add res2tweets

--- functions/src/twitter.ts
+++ functions/src/twitter.ts
@@ -14,6 +14,43 @@ import { Client } from "twitter-api-sdk"
 //   variants?: variants
 // }
 
+const res2tweets = (res: any) => {
+  const media_key2video: {[prop: string]: any} = {}
+  res?.includes?.media && res?.includes?.media.map((media: any) => {
+    if (media.media_key){
+      media_key2video[media.media_key] = media.variants.filter((variant:any) => 'bit_rate' in variant).sort((a: any, b: any) => b.bit_rate - a.bit_rate)[0].url
+    }
+  })
+
+  const authors: {[prop: string]: any} = {}
+  res?.includes?.users?.map((author: any) => {
+    authors[author.id] = author
+  })
+
+  const tweets: any = []
+  res?.data?.map((tweet: any) => {
+    const author = authors[tweet.author_id]
+    tweets.push({
+      id: tweet.id,
+      text: tweet.text,
+      replyCount: tweet.public_metrics?.reply_count ?? 0,
+      twitterLikeCount: tweet.public_metrics?.like_count ?? 0,
+      retweetCount: tweet.public_metrics?.retweet_count ?? 0,
+      name: author?.name,
+      username: author?.username,
+      profileImageUrl: author?.profile_image_url,
+      url: `https://twitter.com/${author?.username}/status/${tweet.id}`,
+      video: tweet.attachments?.media_keys && media_key2video[tweet.attachments?.media_keys[0]],
+      twitterUid: tweet.author_id,
+      likeCount: 0,
+      active: true,
+      publishedAt: admin.firestore.Timestamp.fromDate(new Date(tweet.created_at))
+    })
+  })
+
+  return tweets
+}
+
 export const searchTweets = async () => {
   const startTime = new Date()
   startTime.setMinutes(startTime.getMinutes() - 20)
@@ -42,40 +79,7 @@ export const searchTweets = async () => {
     ]
   })
 
-  const media_key2video: {[prop: string]: any} = {}
-  res?.includes?.media && res?.includes?.media.map((media: any) => {
-    if (media.media_key){
-      media_key2video[media.media_key] = media.variants.filter((variant:any) => 'bit_rate' in variant).sort((a: any, b: any) => b.bit_rate - a.bit_rate)[0].url
-    }
-  })
-
-  const authors: {[prop: string]: any} = {}
-  res?.includes?.users?.map((author: any) => {
-    authors[author.id] = author
-  })
-
-  const tweets: any = []
-  res?.data?.map(async (tweet: any) => {
-    const author = authors[tweet.author_id]
-    tweets.push({
-      id: tweet.id,
-      text: tweet.text,
-      replyCount: tweet.public_metrics?.reply_count ?? 0,
-      twitterLikeCount: tweet.public_metrics?.like_count ?? 0,
-      retweetCount: tweet.public_metrics?.retweet_count ?? 0,
-      name: author?.name,
-      username: author?.username,
-      profileImageUrl: author?.profile_image_url,
-      url: `https://twitter.com/${author?.username}/status/${tweet.id}`,
-      video: tweet.attachments?.media_keys && media_key2video[tweet.attachments?.media_keys[0]],
-      twitterUid: tweet.author_id,
-      likeCount: 0,
-      active: true,
-      publishedAt: admin.firestore.Timestamp.fromDate(new Date(tweet.created_at))
-    })
-  })
-
-  return tweets
+  return res2tweets(res)
 }
 
 export const saveTweets = async (tweets: any) => {
@@ -95,7 +99,7 @@ export const saveTweets = async (tweets: any) => {
 }
 
 
-const deleteTweet = async (targetTweet: any) => {
+const deleteLike = async (targetTweet: any) => {
   const likes = await admin.firestore().collectionGroup('likes').where('tweet.ref', '==', targetTweet.ref).get()
   likes.docs.map(async (doc: any) => {
     console.log('delete!!!!!!!!')
@@ -150,7 +154,7 @@ export const updateAndDelete = async (targetTweets: any) => {
     if (!resTweet){
       functions.logger.info("inactive!!!!!!!!!", {structuredData: true})
       console.log(tweetId)
-      await deleteTweet(targetTweet)
+      await deleteLike(targetTweet)
       tweet.active = false
       tweet.updatedAt = admin.firestore.FieldValue.serverTimestamp()
     }
