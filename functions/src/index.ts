e8d80bc302a91c4eb198509014e6c07afaa47653
sacckey <sackeyad24@gmail.com>
2023-04-09 16:52:07 +0900

    Tweak

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,20 +9,21 @@ export const fetchTweets = functions.region('asia-northeast1').pubsub.schedule('
     const tweets = await searchRecentTweets()
     await createTweets(tweets)
   } catch (e: any) {
+    functions.logger.info("error!")
     functions.logger.error(e)
   }
 
   functions.logger.info("end!")
 })
 
-export const refetchTweets = functions.region('asia-northeast1').pubsub.schedule('0 4 * * *').timeZone('Asia/Tokyo').onRun(async (context) => {
-  functions.logger.info("start!", {structuredData: true})
+export const refetchTweets = functions.region('asia-northeast1').pubsub.schedule('0 4 * * *').timeZone('Asia/Tokyo').onRun(async () => {
+  functions.logger.info("start!")
 
   try {
     const tweetSnapshots = await admin.firestore().collection('tweets').where('active', '==', true).orderBy('publishedAt', 'desc').limit(1000).get()
 
-    const tweetIdsSet:string[][] = []
-    const tweetIds:string[] = []
+    const tweetIdsSet: string[][] = []
+    const tweetIds: string[] = []
     tweetSnapshots.docs.map((doc) => {
       tweetIds.push(doc.id)
 
@@ -37,44 +38,44 @@ export const refetchTweets = functions.region('asia-northeast1').pubsub.schedule
       const tweets = await searchTweetsById(ids)
       await updateTweets(tweets)
     }))
-  } catch (e:any) {
-    console.log('error!!!!')
+  } catch (e: any) {
+    functions.logger.info("error!")
     functions.logger.error(e)
   }
 
-  functions.logger.info("end!", {structuredData: true})
+  functions.logger.info("end!")
 })
 
 export const onCreateLike = functions.region('asia-northeast1').firestore.document('users/{uid}/likes/{tweetId}').onCreate(async (_snapshot, context) => {
-  functions.logger.info("start!", {structuredData: true})
+  functions.logger.info("start!")
 
   try {
     await admin.firestore().collection('tweets').doc(context.params.tweetId).update({
       likeCount: admin.firestore.FieldValue.increment(1),
       updatedAt: admin.firestore.FieldValue.serverTimestamp()
     })
-  } catch (e:any) {
-    console.log('error!!!!')
-    console.error(e)
+  } catch (e :any) {
+    functions.logger.info("error!")
+    functions.logger.error(e)
   }
 
-  functions.logger.info("end!", {structuredData: true})
+  functions.logger.info("end!")
 })
 
 export const onDeleteLike = functions.region('asia-northeast1').firestore.document('users/{uid}/likes/{tweetId}').onDelete(async (_snapshot, context) => {
-  functions.logger.info("start!", {structuredData: true})
+  functions.logger.info("start!")
 
   try {
     await admin.firestore().collection('tweets').doc(context.params.tweetId).update({
       likeCount: admin.firestore.FieldValue.increment(-1),
       updatedAt: admin.firestore.FieldValue.serverTimestamp()
     })
-  } catch (e:any) {
-    console.log('error!!!!')
-    console.error(e)
+  } catch (e: any) {
+    functions.logger.info("error!")
+    functions.logger.error(e)
   }
 
-  functions.logger.info("end!", {structuredData: true})
+  functions.logger.info("end!")
 })
 
 export const saveTestTweets = functions.region('asia-northeast1').https.onCall(async () => {
